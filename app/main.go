92bbdb335f073b4d7e02b38e958865f912dc6418
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-06-02 22:04:30 +0200

    Refactor TCP connection handling in main function

--- app/main.go
+++ app/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
+func connectTcp(address string) *net.TCPListener {
+	ip, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		log.Fatalf("address %s is not valid", address)
+	}
+
+	list, err := net.ListenTCP("tcp", ip)
+	if err != nil {
+		log.Fatalf("failed to listen to %s: %s", ip, err)
+	}
+
+	return list
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l := connectTcp("0.0.0.0:6379")
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		log.Fatalf("Error accepting connection: %s", err)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
