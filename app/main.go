81a282e5f6058cb5bca763c0b353cd130998ff86
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-06-02 22:54:54 +0200

    Refactor TCP connection handling and implement pong function for connection responses

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 )
@@ -19,12 +20,49 @@ func connectTcp(address string) *net.TCPListener {
 	return list
 }
 
+func pong(conn *net.TCPConn, errCh chan error) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			errCh <- err
+		}
+	}()
+
+	buf := make([]byte, 4096)
+	var err error
+	for {
+		n := 0
+		if n, err = conn.Read(buf); err != nil {
+			switch err {
+			case io.EOF:
+				return
+			default:
+				errCh <- err
+			}
+		}
+		log.Println(string(buf[:n]))
+		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+			errCh <- err
+		}
+		// conn.CloseWrite()
+	}
+}
+
 func main() {
 	l := connectTcp("0.0.0.0:6379")
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatalf("Error accepting connection: %s", err)
-	}
 
-	conn.Write([]byte("+PONG\r\n"))
+	errCh := make(chan error, 4)
+
+	go func() {
+		for err := range errCh {
+			log.Fatalf("connection handler error: %s\n", err)
+		}
+	}()
+
+	for {
+		conn, err := l.AcceptTCP()
+		if err != nil {
+			log.Fatalf("Error accepting connection: %s\n", err)
+		}
+		go pong(conn, errCh)
+	}
 }
