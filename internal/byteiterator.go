1f612d91c53dac403b470861df1a04c92f51f3e6
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-06-08 23:10:54 +0200

    Refactor RdbHeader and RdbFile structures, implement ReadRdbHeader and readRdbMetadata functions for improved RDB file handling

--- internal/byteiterator.go
+++ internal/byteiterator.go
@@ -1,20 +1,46 @@
 package internal
 
 import (
+	"bufio"
 	"bytes"
-	"io"
+	"fmt"
 	"os"
 )
 
 type ByteIterator struct {
-	Input  io.Reader
+	buf    *bufio.Reader
 	Offset int
 }
 
-func NewByteIteratorFromBytes(data []byte) *ByteIterator {
-	return &ByteIterator{Input: bytes.NewReader(data), Offset: 0}
+func NewByteIteratorFromBytes(data []byte) (iter *ByteIterator) {
+	iter = &ByteIterator{
+		buf:    bufio.NewReader(bytes.NewReader(data)),
+		Offset: 0,
+	}
+
+	return
 }
 
 func NewByteIteratorFromFile(data *os.File) *ByteIterator {
-	return &ByteIterator{Input: data, Offset: 0}
+	return &ByteIterator{buf: bufio.NewReader(data), Offset: 0}
+}
+
+func (r *ByteIterator) readBytes(n int) ([]byte, error) {
+	buf := make([]byte, n)
+
+	b, err := r.buf.Read(buf)
+	if err != nil {
+		return nil, fmt.Errorf("expected to read %d bytes, got %d", n, b)
+	}
+
+	buf = buf[:b]
+	r.Offset += b
+
+	return buf, err
+}
+
+func (r *ByteIterator) readByte() (byte, error) {
+	r.Offset++
+
+	return r.buf.ReadByte()
 }
--- internal/rdbfile.go
+++ internal/rdbfile.go
@@ -1,5 +1,74 @@
 package internal
 
-type RdbHeader string
+import "fmt"
 
-type RdbFile struct{}
+type RdbHeader struct {
+	magic   string
+	version string
+}
+
+const (
+	DEFAULT_HEADER_MAGIC   = "REDIS"
+	HEADER_MAGIC_BYTES     = 5
+	DEFAULT_HEADER_VERSION = "0011"
+	HEADER_VERSION_BYTES   = 4
+	HEADER_BYTES           = HEADER_MAGIC_BYTES + HEADER_VERSION_BYTES
+	METADATA_FIELD         = 0xFA
+)
+
+func NewRdbHeader() RdbHeader {
+	return RdbHeader{
+		magic:   DEFAULT_HEADER_MAGIC,
+		version: DEFAULT_HEADER_VERSION,
+	}
+}
+
+func ReadRdbHeader(iter *ByteIterator) (h RdbHeader, err error) {
+	data, err := iter.readBytes(HEADER_MAGIC_BYTES)
+	if err != nil {
+		return
+	}
+
+	h.magic = string(data)
+
+	data, err = iter.readBytes(HEADER_VERSION_BYTES)
+	if err != nil {
+		return
+	}
+
+	h.version = string(data)
+
+	return
+}
+
+type RdbMetadata map[string]string
+
+func readRdbMetadata(iter *ByteIterator) (meta RdbMetadata, err error) {
+	if meta_field, err := iter.readByte(); err != nil {
+		return nil, err
+	} else if meta_field != METADATA_FIELD {
+		return nil, fmt.Errorf("expected field %X, got %X", METADATA_FIELD, meta_field)
+	}
+
+	return
+}
+
+type RdbFile struct {
+	header   RdbHeader
+	metadata RdbMetadata
+	selector int
+}
+
+func ReadRdbFile(iter *ByteIterator) (rdb *RdbFile, err error) {
+	rdb = &RdbFile{}
+
+	if rdb.header, err = ReadRdbHeader(iter); err != nil {
+		return nil, fmt.Errorf("error reading rdb header: %w", err)
+	}
+
+	if rdb.metadata, err = readRdbMetadata(iter); err != nil {
+		return nil, fmt.Errorf("error reading rdb metadata: %w", err)
+	}
+
+	return
+}
