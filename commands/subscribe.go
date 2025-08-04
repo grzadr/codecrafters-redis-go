e5c58ee96d1a45ee49b0992c0ff43e7b704401b7
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-08-04 14:57:55 +0200

    Refactor CmdSubscribe: remove commented-out code and clean up Exec method

--- commands/subscribe.go
+++ commands/subscribe.go
@@ -1,10 +1,6 @@
 package commands
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/codecrafters-io/redis-starter-go/pubsub"
 	"github.com/codecrafters-io/redis-starter-go/rheltypes"
 )
 
@@ -19,22 +15,19 @@ func NewCmdSubscribe() CmdSubscribe {
 func (c CmdSubscribe) Exec(
 	args rheltypes.Array,
 ) (value rheltypes.RhelType, err error) {
-	key := args.At(0).String()
-
-	// timeout, _ := args.At(1).Float()
-
-	last, err := pubsub.ReadLast(key, int(timeout*milisecondInSecond))
-
-	if err != nil {
-		return nil, c.ErrWrap(fmt.Errorf("failed to read last: %w", err))
-	} else if last == nil {
-		log.Println("returning null string")
-
-		return rheltypes.NewNullBulkString(), nil
-	} else {
-		return rheltypes.Array{
-			rheltypes.NewBulkString(key),
-			last,
-		}, nil
-	}
+	// key := args.At(0).String()
+	// // timeout, _ := args.At(1).Float()
+	// last, err := pubsub.ReadLast(key, int(timeout*milisecondInSecond))
+	// if err != nil {
+	// 	return nil, c.ErrWrap(fmt.Errorf("failed to read last: %w", err))
+	// } else if last == nil {
+	// 	log.Println("returning null string")
+	// 	return rheltypes.NewNullBulkString(), nil
+	// } else {
+	// 	return rheltypes.Array{
+	// 		rheltypes.NewBulkString(key),
+	// 		last,
+	// 	}, nil
+	// }
+	return nil, nil
 }
